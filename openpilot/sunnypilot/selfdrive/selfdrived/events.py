7c6d07b52cbc24ab3caeb591f1229d1283c32cbf
davkg <3615426+davkg@users.noreply.github.com>
2026-08-25 19:35:21 -0500

    add beep for exp mode switched

--- openpilot/sunnypilot/selfdrive/selfdrived/events.py
+++ openpilot/sunnypilot/selfdrive/selfdrived/events.py
@@ -9,7 +9,7 @@ from openpilot.cereal import log, custom
 from opendbc.car.structs import car
 from openpilot.common.constants import CV
 from openpilot.sunnypilot.selfdrive.selfdrived.events_base import EventsBase, Priority, ET, Alert, \
-  NoEntryAlert, ImmediateDisableAlert, EngagementAlert, NormalPermanentAlert, AlertCallbackType, wrong_car_mode_alert
+  NoEntryAlert, ImmediateDisableAlert, EngagementAlert, AlertCallbackType, wrong_car_mode_alert
 from openpilot.sunnypilot.selfdrive.controls.lib.speed_limit import PCM_LONG_REQUIRED_MAX_SET_SPEED, CONFIRM_SPEED_THRESHOLD
 from openpilot.common.hardware import HARDWARE
 
@@ -182,7 +182,10 @@ EVENTS_SP: dict[int, dict[str, Alert | AlertCallbackType]] = {
   },
 
   EventNameSP.experimentalModeSwitched: {
-    ET.WARNING: NormalPermanentAlert("Experimental Mode Switched", duration=1.5)
+    ET.PERMANENT: Alert(
+      "Experimental Mode Switched", "",
+      AlertStatus.normal, AlertSize.small,
+      Priority.LOWER, VisualAlert.none, AudibleAlertSP.promptSingleHigh, 1.5),
   },
 
   EventNameSP.wrongCarModeAlertOnly: {
